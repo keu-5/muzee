948e2272b0c3f1c14c016781d3373d9b243a1ee0
keu-5 <keu02.78@gmail.com>
2025-09-13 00:38:23 +0900

    feat: add JWT middleware for user authentication

--- backend/internal/middleware/jwt.go
+++ backend/internal/middleware/jwt.go
@@ -1,1 +1,15 @@
 package middleware
+
+import (
+	"github.com/gofiber/fiber/v2"
+	jwtware "github.com/gofiber/jwt/v3"
+)
+
+var jwtSecret = []byte("supersecret")
+
+func JWTProtected() fiber.Handler {
+	return jwtware.New(jwtware.Config{
+		SigningKey: jwtSecret,
+		ContextKey: "user",
+	})
+}
