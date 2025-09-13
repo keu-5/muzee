bcc061598c29bff52a874e715c9b00280ddac2fa
keu-5 <keu02.78@gmail.com>
2025-09-13 17:32:16 +0900

    feat: refactor database and migration functions to use logger for error handling

--- backend/internal/infrastructure/database.go
+++ backend/internal/infrastructure/database.go
@@ -1,17 +1,15 @@
 package infrastructure
 
 import (
-	"log"
-
 	"entgo.io/ent/dialect"
 	"github.com/keu-5/muzee/backend/config"
 	"github.com/keu-5/muzee/backend/ent"
 )
 
-func NewClient(cfg *config.Config) *ent.Client {
+func NewClient(cfg *config.Config, logger *Logger) *ent.Client {
 	client, err := ent.Open(dialect.Postgres, cfg.DSN())
 	if err != nil {
-		log.Fatalf("failed opening connection to postgres: %v", err)
+		logger.Fatalf("failed opening connection to postgres: %v", err)
 	}
 	return client
 }
--- backend/internal/infrastructure/migration.go
+++ backend/internal/infrastructure/migration.go
@@ -2,17 +2,16 @@ package infrastructure
 
 import (
 	"context"
-	"log"
 
 	"github.com/keu-5/muzee/backend/ent"
 	"go.uber.org/fx"
 )
 
-func AutoMigrate(lc fx.Lifecycle, client *ent.Client) {
+func AutoMigrate(lc fx.Lifecycle, client *ent.Client, logger *Logger) {
 	lc.Append(fx.Hook{
 		OnStart: func(ctx context.Context) error {
 			if err := client.Schema.Create(ctx); err != nil {
-				log.Fatalf("failed creating schema resources: %v", err)
+				logger.Fatalf("failed creating schema resources: %v", err)
 			}
 			return nil
 		},
