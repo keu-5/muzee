54fb442dd2e7177277c210736a549222cc8742b4
keu-5 <keu02.78@gmail.com>
2025-09-14 16:47:59 +0900

    feat: add Header and Page components with Storybook stories
    - Created Header component with login/logout functionality.
    - Added Page component to demonstrate usage of Header.
    - Implemented Storybook stories for both Header and Page components.
    - Included CSS styles for Header, Page, and Button components.
    - Added various assets including SVGs and images for Storybook.
    - Configured Vitest for testing Storybook stories.

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
 import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
@@ -18,137 +21,131 @@ const compat = new FlatCompat({
   allConfig: js.configs.all,
 });
 
-export default [
-  {
-    ignores: [
-      "coverage",
-      ".storybook",
-      ".next",
-      "*.config.mjs",
-      "tailwind.config.ts",
-      "src/types/generatedSchema.ts",
-      "src/api/__generated__/**/*",
-      "components/*.{ts,tsx}",
-    ],
+export default [{
+  ignores: [
+    "coverage",
+    ".storybook",
+    ".next",
+    "*.config.mjs",
+    "tailwind.config.ts",
+    "src/types/generatedSchema.ts",
+    "src/api/__generated__/**/*",
+    "components/*.{ts,tsx}",
+  ],
+}, ...fixupConfigRules(
+  compat.extends(
+    "plugin:@typescript-eslint/recommended",
+    "next/core-web-vitals",
+    "plugin:import/recommended",
+    "plugin:import/warnings",
+    "plugin:tailwindcss/recommended",
+    "prettier"
+  )
+), {
+  plugins: {
+    "@typescript-eslint": fixupPluginRules(typescriptEslint),
+    "simple-import-sort": simpleImportSort,
+    "unused-imports": unusedImports,
+    "import-access": importAccess,
+    "readable-tailwind": readableTailwind,
   },
-  ...fixupConfigRules(
-    compat.extends(
-      "plugin:@typescript-eslint/recommended",
-      "next/core-web-vitals",
-      "plugin:import/recommended",
-      "plugin:import/warnings",
-      "plugin:tailwindcss/recommended",
-      "prettier"
-    )
-  ),
-  {
-    plugins: {
-      "@typescript-eslint": fixupPluginRules(typescriptEslint),
-      "simple-import-sort": simpleImportSort,
-      "unused-imports": unusedImports,
-      "import-access": importAccess,
-      "readable-tailwind": readableTailwind,
+  languageOptions: {
+    parser: tsParser,
+    ecmaVersion: "latest",
+    sourceType: "module",
+    parserOptions: {
+      project: "./tsconfig.json",
+      tsconfigRootDir: __dirname,
     },
-    languageOptions: {
-      parser: tsParser,
-      ecmaVersion: "latest",
-      sourceType: "module",
-      parserOptions: {
-        project: "./tsconfig.json",
-        tsconfigRootDir: __dirname,
+  },
+  settings: {
+    tailwindcss: {
+      callees: ["cn", "cva"],
+    },
+  },
+  rules: {
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: "variable",
+        types: ["array", "boolean", "number", "string"],
+        format: ["strictCamelCase", "UPPER_CASE"],
       },
-    },
-    settings: {
-      tailwindcss: {
-        callees: ["cn", "cva"],
+      {
+        selector: "variable",
+        types: ["function"],
+        format: ["strictCamelCase", "StrictPascalCase"],
       },
-    },
-    rules: {
-      "@typescript-eslint/naming-convention": [
-        "error",
-        {
-          selector: "variable",
-          types: ["array", "boolean", "number", "string"],
-          format: ["strictCamelCase", "UPPER_CASE"],
-        },
-        {
-          selector: "variable",
-          types: ["function"],
-          format: ["strictCamelCase", "StrictPascalCase"],
-        },
-      ],
-      "simple-import-sort/imports": "error",
-      "simple-import-sort/exports": "error",
-      "import/first": "error",
-      "import/newline-after-import": "error",
-      "import/no-duplicates": "error",
-      "@typescript-eslint/consistent-type-exports": "error",
-      "import/group-exports": "error",
-      "unused-imports/no-unused-imports": "error",
-      "import-access/jsdoc": ["error"],
-      "no-restricted-imports": [
-        "error",
-        {
-          paths: [
-            "sonner",
-            "next/link",
-            "react-icons",
-            "lucide-react",
-            "zod",
-            { name: "@/components/ui/Form", importNames: ["Form"] },
-            {
-              name: "@next/third-parties/google",
-              importNames: ["sendGAEvent"],
-            },
-          ],
-          patterns: ["react-icons/*"],
-        },
-      ],
-      "tailwindcss/no-custom-classname": [
-        "error",
-        {
-          classRegex:
-            "^(class(Name)?|textClassName|iconClassName|innerClassName)$",
-          whitelist: ["^[A-Z].*"],
-        },
-      ],
-      "readable-tailwind/multiline": [
-        "warn",
-        {
-          group: "newLine",
-        },
-      ],
-      "no-restricted-syntax": [
-        "error",
-        {
-          selector:
-            "CallExpression[callee.object.name='Object'][callee.property.name='keys']",
-          message:
-            "Do not use Object.keys. Check src/utils/object.ts or add a new utility function.",
-        },
-      ],
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-unnecessary-type-assertion": "error",
-    },
-  },
-  {
-    files: [
-      "src/**/*.stories.tsx",
-      "src/**/*Type.ts",
-      "src/types/**",
-      "src/features/**/*Repository.ts",
-      "src/features/**/*Converter.ts",
-      "src/features/**/*Constants.ts",
-      "src/api/__generated__/**/*",
     ],
-    rules: {
-      "import/group-exports": "off",
-    },
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error",
+    "@typescript-eslint/consistent-type-exports": "error",
+    "import/group-exports": "error",
+    "unused-imports/no-unused-imports": "error",
+    "import-access/jsdoc": ["error"],
+    "no-restricted-imports": [
+      "error",
+      {
+        paths: [
+          "sonner",
+          "next/link",
+          "react-icons",
+          "lucide-react",
+          "zod",
+          { name: "@/components/ui/Form", importNames: ["Form"] },
+          {
+            name: "@next/third-parties/google",
+            importNames: ["sendGAEvent"],
+          },
+        ],
+        patterns: ["react-icons/*"],
+      },
+    ],
+    "tailwindcss/no-custom-classname": [
+      "error",
+      {
+        classRegex:
+          "^(class(Name)?|textClassName|iconClassName|innerClassName)$",
+        whitelist: ["^[A-Z].*"],
+      },
+    ],
+    "readable-tailwind/multiline": [
+      "warn",
+      {
+        group: "newLine",
+      },
+    ],
+    "no-restricted-syntax": [
+      "error",
+      {
+        selector:
+          "CallExpression[callee.object.name='Object'][callee.property.name='keys']",
+        message:
+          "Do not use Object.keys. Check src/utils/object.ts or add a new utility function.",
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-unnecessary-type-assertion": "error",
   },
-  {
-    files: ["src/components/**/*.{ts,tsx}"],
-    rules: {
-      "no-restricted-imports": "off",
-    },
+}, {
+  files: [
+    "src/**/*.stories.tsx",
+    "src/**/*Type.ts",
+    "src/types/**",
+    "src/features/**/*Repository.ts",
+    "src/features/**/*Converter.ts",
+    "src/features/**/*Constants.ts",
+    "src/api/__generated__/**/*",
+  ],
+  rules: {
+    "import/group-exports": "off",
   },
-];
+}, {
+  files: ["src/components/**/*.{ts,tsx}"],
+  rules: {
+    "no-restricted-imports": "off",
+  },
+}, ...storybook.configs["flat/recommended"]];
