d86dfbc1e532934e811b687455686052deed9225
keu-5 <keu02.78@gmail.com>
2025-11-05 23:53:37 +0900

    feat: edit user profile schema

--- backend/ent/schema/userprofile.go
+++ backend/ent/schema/userprofile.go
@@ -1,6 +1,13 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
+)
 
 // UserProfile holds the schema definition for the UserProfile entity.
 type UserProfile struct {
@@ -9,10 +16,46 @@ type UserProfile struct {
 
 // Fields of the UserProfile.
 func (UserProfile) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.Int64("id"),
+
+		field.String("name").
+			MaxLen(100).
+			NotEmpty(),
+
+		field.String("username").
+			MaxLen(50).
+			NotEmpty().
+			Unique(),
+
+		field.String("icon_path").
+			MaxLen(255).
+			Optional().
+			Nillable(),
+
+		field.Time("created_at").
+			Default(time.Now).
+			Immutable(),
+
+		field.Time("updated_at").
+			Default(time.Now).
+			UpdateDefault(time.Now),
+	}
 }
 
 // Edges of the UserProfile.
 func (UserProfile) Edges() []ent.Edge {
-	return nil
+	return []ent.Edge{
+		edge.From("user", User.Type).
+			Ref("profile").
+			Unique().
+			Required(),
+	}
+}
+
+func (UserProfile) Indexes() []ent.Index {
+	return []ent.Index{
+		index.Fields("username").Unique(),
+		index.Fields("created_at"),
+	}
 }
